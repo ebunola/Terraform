b8c88080380bfc60c84fbc1e00128cdb25e5248c
ebunola <ebunoluwabiona@outlook.com>
2023-03-10 01:18:54 +0100

    update for Windows variable

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -7,7 +7,9 @@ terraform {
   }
 }
 
-provider "docker" {}
+provider "docker" {
+   host    = "npipe:////.//pipe//docker_engine"
+}
 
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
